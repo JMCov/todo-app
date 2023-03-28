9b5bfbada112833072839eb10706bf807d315ee2
Jordan Covington <Atlantafan1087@gmail.com>
2023-03-28 09:31:47 -0700

    added some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import Todo from './Components/Todo';
+import Footer from './Components/Footer';
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <Todo />
+      <>
+        <Todo />
+        <Footer />
+      </>
     );
   }
 }
--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Button, Card, Text } from '@mantine/core';
+import './list.css';
 
 const List = ({ display, toggleComplete }) => {
   return (
-
-    <div>
-      {display.map(item => (
-        <div key={item.id}>
-          <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-          <hr />
-      </div>
-    ))}
-  </div>
+    <div className="list-container">
+      {display.map((item) => (
+        <Card key={item.id} shadow="sm" className="list-card">
+          <div className="list-header">
+            <Text size="sm" className="list-assignee">{item.assignee}</Text>
+            <Button variant="subtle" className="list-close-button" onClick={() => toggleComplete(item.id)}>X</Button>
+          </div>
+          <div className="list-body">
+            <Text size="lg" className="list-item-text">{item.text}</Text>
+            <Text size="sm" className="list-difficulty">{`Difficulty: ${item.difficulty}`}</Text>
+          </div>
+        </Card>
+      ))}
+    </div>
   );
 };
 
